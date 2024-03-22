b84dd2d6f2fb19cebd1ef68797333dc84dabf632
Leari John Barasona <learijohnb@gmail.com>
2024-03-23 06:25:53 +0800

    Fix error on Herd Classification

--- API-PCC/ApplicationModels/HerdSearchFilterModel.cs
+++ API-PCC/ApplicationModels/HerdSearchFilterModel.cs
@@ -6,8 +6,8 @@
         public FilterByModel? filterBy { get; set; }
         public int page { get; set; }
         public int pageSize { get; set; }
-        public DateOnly dateFrom { get; set; }
-        public DateOnly dateTo { get; set; }
+        public DateTime dateFrom { get; set; }
+        public DateTime dateTo { get; set; }
         public SortByModel sortBy { get; set; }
         
     }
